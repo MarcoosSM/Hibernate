2bd5f676ef6ea1d57ed15da632ccf49fe80e8063
MiguelAngelDAM <44416407+MiguelAngelDAM@users.noreply.github.com>
2019-01-14 17:29:25 +0100

    Añadidas librerias, conector y cambio de interfaz

--- Acceso_a_Datos_Practica_2/src/interfaces/ApplicationWindow.java
+++ Acceso_a_Datos_Practica_2/src/interfaces/ApplicationWindow.java
@@ -36,7 +36,7 @@ public class ApplicationWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 600, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
